28075fc8c016fdf6c0c6f62e7bb813c98af82aba
Yuxiao Tian <jameslikelywood@gmail.com>
2024-09-16 00:48:49 -0400

    1/4 of way go

---
+++ aeroplan/article/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+# content page
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    
